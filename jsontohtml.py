24e9f2eb103037586e97301dd646f92236b7f3a3
Harshita Sahai <40337803+HarshitaSahai@users.noreply.github.com>
2020-05-23 16:06:14 +0530

    Update jsontohtml.py

--- jsontohtml.py
+++ jsontohtml.py
@@ -1,24 +1,23 @@
+#Converting json to html 
+
 from json2table import *
 import json
-
-
-
 
 data = open('free.json','r')
 jsonFile = data.read()
 foo = json.loads(jsonFile)
 build_dir = "LEFT_TO_RIGHT"
-table_attr = {"style" : "width:100%", "class" : "table table-striped","border": "1px solid black"}
+table_attr = {"style" : "width:100%", "class" : "table table-striped","border": "1px solid black"} # Table style
 html = convert(foo, build_direction=build_dir,table_attributes=table_attr)
 
-with open("free.html", "w") as ht:
-    ht.write(html)
+with open("free.html", "w") as ht: 
+    ht.write(html)              
 
 data = open('paid.json','r')
 jsonFile = data.read()
 foo = json.loads(jsonFile)
 build_dir = "LEFT_TO_RIGHT"
-table_attr = {"style" : "width:100%", "class" : "table table-striped","border": "1px solid black"}
+table_attr = {"style" : "width:100%", "class" : "table table-striped","border": "1px solid black"} # Table style
 html = convert(foo, build_direction=build_dir,table_attributes=table_attr)
 
 with open("paid.html", "w") as ht:
